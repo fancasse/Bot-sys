a3af07631f3bd32a78df5c3084ebdedafa34da2a
fancasse <49524037+fancasse@users.noreply.github.com>
2020-04-12 16:11:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 ﻿const Discord = require('discord.js');
-const devs = ['565995291615166507'];
+const devs = ['389090790984515594'];
 const db = require('quick.db');
-const premium = ['565995291615166507']
+const premium = ['470896018603376640']
 const client = new Discord.Client();   
 const bot = new Discord.Client();   
 const giphy = require('giphy-api')();    
